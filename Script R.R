e499b23e6ea1943e6f84dc79b87f8623e0a919a2
Rhia <phyrxxne@outlook.com>
2023-05-27 18:37:40 -0400

    Update Code

--- Script R.R
+++ Script R.R
@@ -1,14 +1,15 @@
-# ---------------INSTALACIÓN DE PAQUETES DE INVESTIGACIÓN----------------
+# ---------------INSTALACIÓN DE PAQUETES DE INVESTIGACIÓN------------------
 install.packages("ggplot2",
                  "dplyr",
                  "stringr",
                  "lubridate",
                  "forecast",
                  "stargazer")
-#----------------CARGAR LIBRERIAS DE PAQUETES DE INVESTIGACIÓN
+#----------------CARGAR LIBRERIAS DE PAQUETES DE INVESTIGACIÓN-------------
 library(ggplot2)
 library(dplyr)
 library(stringr)
 library(lubridate)
 library(forecast)
 library(stargazer)
+#---------------DESARROLLO DEL CÓDIGO PARA LA INVESTIGACIÓN----------------
