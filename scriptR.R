525dbeda5c6dcd89066bafc6b08a6aff10bffa3b
Rhia-nnon <134813267+Rhia-nnon@users.noreply.github.com>
2023-07-07 20:28:45 -0400

    Update

--- scriptR.R
+++ scriptR.R
@@ -10,7 +10,6 @@
 sessionInfo()
 options(scipen = 99999)
 
-
 #------------------CARGAR LIBRERIAS DE PAQUETES DE INVESTIGACIÓN----------------
 library(tidyverse)
 library(stargazer)
@@ -34,7 +33,10 @@ HitesEstResultados <- read_excel("HITES.xlsx", sheet = "resultado1")
 View(HitesEstResultados)
 
 ls() # Revisa que todo esté cargado
-#------------------ANÁLISIS DEL DATASET DE ACTIVOS DE HITES---------------------
+
+
+#-------------------ANÁLISIS DE Los DATASETS DE HITES S.A.---------------------#
+
 # Funciones que permiten identificar la estructura del Dataset
 head(HitesActivo)
 tail(HitesActivo)
@@ -47,11 +49,8 @@ ncol(HitesActivo)
 ls(HitesActivo)
 names(HitesActivo)
 summary(HitesActivo)
-
-# Limpieza del Dataset
-
-as.Date(HitesActivo$Fechas)
-glimpse(HitesActivo)
+glimpse(summary(HitesActivo))
+names(HitesActivo)
 
 # Tendencia Central
 mean(HitesActivo$`Activo total`)
@@ -61,6 +60,16 @@ median(HitesActivo$`Activo total`)
 range(HitesActivo$`Activo total`)
 var(HitesActivo$`Activo total`)
 sd(HitesActivo$`Activo total`)
+
+#------------------------------LIMPIEZA DEL DATASET-----------------------------
+
+
+#------------------------------ANALISIS DE DATOS--------------------------------
+#--------------------------RATIOS FINANCIEROS-----------------------------------
+
+
+
+#------------------ANALISIS DEL DATASET DE ACTIVOS DE HITES S.A.----------------
 
 # Evolución del Activo Total de Hites S.A.
 ggplot(data = HitesActivo, aes(y = `Activo total`, x = Fechas)) +
@@ -92,43 +101,71 @@ ggplot(data = HitesActivo, aes(y = `Activo total`, x = Fechas)) +
       )
   ) + scale_x_datetime(date_labels = "%Y-%m-%d", date_breaks = "1 year")
 
+# Ajustamos la fecha del dataset al formato de la ISO 8601
+HitesActivo$Fechas = as.Date(HitesActivo$Fechas, format = "%Y/%m/%d")
+
+# Guardamos la secuencia de tiempo, la frecuencia indica que es trimestral,
+# y que es 4 veces al año, empieza el primer trimestre del 2010 y termina el
+# primer trimestre del 2023
+
+Efectivo_fcst = ts(
+  HitesActivo$Efectivo,
+  start = c(2010, 1),
+  end = c(2023, 1),
+  frequency = 4
+)
+
+# Muestra de resultados
+print(Efectivo_fcst)
+
+# Generamos un pronóstico para la evolución de los activos totales
+autoplot(
+  forecast(Efectivo_fcst),
+  xlab = "Tiempo",
+  ylab = "Monto",
+  main = "Pronóstico para la cuenta Efectivo de Hites S.A."
+) + scale_y_continuous(
+  labels = function(x)
+    format(x, big.mark = ".", scientific = FALSE)
+) +  scale_x_continuous(
+  breaks = c(
+    2010,
+    2011,
+    2012,
+    2013,
+    2014,
+    2015,
+    2016,
+    2017,
+    2018,
+    2019,
+    2020,
+    2021,
+    2022,
+    2023,
+    2024,
+    2025
+  )
+)
+
 #-----------ANALISIS DEL DATASET DE PASIVOS DE HITES S.A.-----------------------
 
 #-----------ANALISIS DEL DATASET DE PATRIMONIO DE HITES S.A.--------------------
 
 #-----------ANALISIS DEL DATASET DE ESTADO DE RESULTADOS DE HITES S.A.----------
 
-# Extraemos los ingresos netos al cierre de cada periodo del dataset
-Result_neto2 <- as.integer(
-  c(
-    183910997,
-    227512020,
-    253830651,
-    271056964,
-    275077376,
-    280349317,
-    302489675,
-    311870350,
-    316708928,
-    324743257,
-    291189414,
-    403820306,
-    368393931
-  )
-)
+# Extraemos los Ingresos netos, partiendo por el año 2010 con una frecuencia de
+# cuatro veces por año.
 
-# Creamos un vector con todas las fechas de cierre entre el 2010 y el 2022
-Result_fecha <-
-  seq(as.Date("2010-12-31"), as.Date("2022-12-31"), by = "1 year")
+Ing_net_ts <-
+  ts(HitesEstResultados$`+Ingresos netos...5`, 2010, frequency = 4)
 
-
-# Las unimos para tener un vector que muestre los resultados netos al cierre de cada periodo
-Resultados_df <- data.frame(Result_fecha,
-                            Result_neto2)
-print(Resultados_df)
+# Imprimimos los datos antes de modelarlos
+print(Ing_net_ts)
 
 # Gráfico de la evolución de los ingresos netos de Hites S.A.
-ggplot(data = Resultados_df, aes(y = Result_neto2, x = Result_fecha)) +
+ggplot(data = HitesEstResultados, aes(y = `+Ingresos netos...5`,
+                                      x = Fechas)) +
   geom_line(col = "#addd8e", size = 0.5) +  geom_point(col = "#85aa6d", size = 2) +
   ggtitle("Evolución de los Ingresos Netos de Hites S.A.",
           subtitle = "Corresponde a las variaciones en el monto para los intervalos entre el año 2010 y 2022") +
@@ -143,19 +180,22 @@ ggplot(data = Resultados_df, aes(y = Result_neto2, x = Result_fecha)) +
       size = 8,
       face = "italic",
       vjust = -1
-    )) + xlab("Fecha de corte") + 
-  ylab("Monto")  + scale_x_date(date_labels = "%Y-%m-%d", date_breaks = "1 year")
+    )
+  ) + xlab("Fecha de corte") +
+  ylab("Monto")  + scale_x_datetime(date_labels = "%Y-%m-%d", date_breaks = "years")
 
-# Pronósticos de Ingresos para Hites S.A.
-fcst_rneto <- forecast(Result_neto2)
+# Generamos un gráfico que muestra un pronóstico para el movimiento de los
+# ingresos netos de Hites S.A.
 
-print(fcst_rneto)
+autoplot(forecast(Ing_net_ts),
+         xlab = "Periodo",
+         ylab = "Monto",
+         main = "Pronóstico para los Ingresos Netos de Hites S.A.") +
+  scale_y_continuous(
+    labels = function(x)
+      format(x, big.mark = ".", scientific = FALSE)
+  )
 
-autoplot(
-  fcst_rneto,
-  xlab = "Fecha",
-  ylab = "Monto",
-  na.rm = TRUE) +
-  ggtitle("Pronóstico de la evolución de los Ingresos Netos de Hites S.A.",
-          subtitle = "Gráfico elaborado en base a modelos ARIMA")
-
+#--------------------------------FINAL------------------------------------------
+# Liberación de memoria
+gc()
